1dcee7c75a36ae46db6e8daddb2b071b58be1e00
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-09-23 18:18:46 +0530

    Dump2

--- src/component/Nav/NavLinks/NavText/NavText.jsx
+++ src/component/Nav/NavLinks/NavText/NavText.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavText = () => {
   return (
-    <div className="absolute top-20 left-0 z-40 bg-white bg-opacity-70 p-8 max-w-2xl  mt-20 md:mt-32 lg:mt-40 lg:ml-24">
+    <div className="absolute top-20 left-0 z-40 bg-white bg-opacity-70 p-8 max-w-xl  mt-20 md:mt-32 lg:mt-40 ">
       {/* Heading */}
       <h2 className="text-4xl md:text-5xl font-bold text-pink-950 mb-4">Welcome</h2>
       
