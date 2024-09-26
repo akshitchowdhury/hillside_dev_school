05e6c1e871d44b1d1ac254ad70d0f95533533bad
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-09-26 15:14:21 +0530

    CareerLink

--- src/component/Home/HomeApply.jsx
+++ src/component/Home/HomeApply.jsx
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom'
 
 
 export default function HomeApply() {
+  const handleClick = () => {
+    window.scrollTo({ behavior: 'smooth', top: "0" });
+  };
   return (
     <div className="bg-[#f1be31] text-pink-950 p-6 md:p-8 lg:p-10">
       <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-4">
@@ -12,7 +15,7 @@ export default function HomeApply() {
         Great teachers love what they do and create great experiences for their students. The IHS family welcomes teachers who can open eyes and minds to innovation. Are you that kind of teacher?
       </p>
       <Link
-        to="/apply"
+        to="/career" onClick={handleClick}
         className="text-sm md:text-base text-white lg:text-lg underline hover:no-underline"
       >
         Apply via portal
